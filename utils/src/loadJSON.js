eb9422148712da1a0495b3a6b4c1c54449fe90bf
Demian <ddrozdov@oskelly.ru>
2022-12-28 03:05:58 +0700

    update utils

--- utils/src/loadJSON.js
+++ utils/src/loadJSON.js
@@ -5,7 +5,7 @@ export default async function loadJSON (file) {
   const filePath = path.resolve('./', file);
 
   try {
-    // https://nodejs.org/dist/latest-v16.x/docs/api/esm.html#no-json-module-loading
+    // https://nodejs.org/dist/latest-v16.x/docs/api/esm.html#no-json-module-loading //
     const json = JSON.parse(
       await readFile(new URL(filePath, import.meta.url))
     );
