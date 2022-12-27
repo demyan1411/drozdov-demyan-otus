1dadb09d7bad5bbe7d16a569cfd9abbe522d635f
Demian <ddrozdov@oskelly.ru>
2022-12-28 04:16:25 +0700

    update

--- hm-1/src/index.js
+++ hm-1/src/index.js
@@ -1,7 +1,5 @@
-import { loadJSON } from '@demdjanium/utils';
-
+import { loadJSON, logTree } from '@demdjanium/utils';
 import { commands } from './commander.js';
-import logTree from './logTree.js';
 
 const json = await loadJSON(`./${commands.filename}`);
 
