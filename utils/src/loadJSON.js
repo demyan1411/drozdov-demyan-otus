ea4b59ff65de2ecabbeac7f9554b3485aa3a2183
Demian <ddrozdov@oskelly.ru>
2022-12-29 20:57:55 +0700

    review

--- utils/src/loadJSON.js
+++ utils/src/loadJSON.js
@@ -1,15 +1,21 @@
 import path from 'path';
-import { readFile } from 'node:fs/promises';
+// import { readFile } from 'node:fs/promises';
 
 export default async function loadJSON (file) {
   const filePath = path.resolve('./', file);
 
   try {
-    const json = JSON.parse(
-      await readFile(new URL(filePath, import.meta.url), { encoding: 'utf8' })
-    );
+    const { default: jsonData } = await import(filePath, {
+      assert: {
+        type: 'json'
+      },
+    });
 
-    return json;
+    // const jsonData = JSON.parse(
+    //   await readFile(new URL(filePath, import.meta.url), { encoding: 'utf8' })
+    // );
+
+    return jsonData;
   } catch (error) {
     throw new Error(error);
   }
